122ce20db2e2c2027eb9eefb2dd239c3d8cf25b8
Ankita-singh-05 <asankitasingh2017@gmail.com>
2021-12-25 23:55:40 +0530

    Hero Carousal

--- src/components/HeroCarousal/HeroCarousal.components.js
+++ src/components/HeroCarousal/HeroCarousal.components.js
@@ -7,7 +7,6 @@ import { PrevArrow, NextArrow } from "./Arrows.components";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 
 const HeroCarousal = () => {
     const settings = {
@@ -16,7 +15,6 @@ const HeroCarousal = () => {
         centerPadding: "160px",
         dots: true,
         infinite: true,
-        speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         prevArrow: <PrevArrow />,
