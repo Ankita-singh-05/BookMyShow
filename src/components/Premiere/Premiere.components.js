7c2060acad44302dbd10bcfcf4638373d989ee4c
Ankita-singh-05 <asankitasingh2017@gmail.com>
2021-12-25 14:26:49 +0530

    Premiere Component

--- src/components/Premiere/Premiere.components.js
+++ src/components/Premiere/Premiere.components.js
@@ -4,18 +4,97 @@ import Slider from "react-slick";
 //Poster
 import Poster from "../Poster/poster.components";
 
-export const Premiere = () => {
+const Premiere = () => {
     const settings = {
         infinity: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToShow: 5,
+        slidesToScroll: 2,
         autoplay: false,
         InitialSlide: 0
     };
 
+    const PremiereImages = [
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        },
+        {
+            src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00020507-hvddmzbcer-portrait.jpg",
+            alt: "SON of GOD",
+            title: "SON OF GOD",
+            subtitle: "English",
+            isDark: false
+        }
+    ];
+
     return (
         <>
-            <Slider {...settings} />
+            <Slider {...settings} >
+            {PremiereImages.map((image) => (
+                <Poster {...image} />
+            ))}
+            </Slider>
         </>
     );
 };
+
+export default Premiere;
--- src/pages/Home.pages.js
+++ src/pages/Home.pages.js
@@ -1,6 +1,7 @@
 import React from "react";
 //Components
 import EntertainmentCardSlider from "../components/EntertainmentCard/EntertainmentCard.component";
+import  Premiere  from "../components/Premiere/Premiere.components";
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
                     The Best of Entertainment
                 </h1>
                 <EntertainmentCardSlider />
+                <Premiere />
             </div>
         </>
     );
