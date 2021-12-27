6d8b7b2e9a3d3d318e81e1a34fffabfe2e06a23c
Ankita-singh-05 <asankitasingh2017@gmail.com>
2021-12-27 17:17:54 +0530

    movie page banner

--- src/pages/Movie.pages.js
+++ src/pages/Movie.pages.js
@@ -1,11 +1,22 @@
 const Movie  = () => {
     return (
         <>  
+        <div className="">
             <div className="w-full h-full">
                 <img src="https://in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/the-matrix-resurrections-et00133391-24-11-2021-12-59-51.jpg" 
                       className="w-full h-full"
                 />
             </div>
+
+                <div className="absolute z-1">
+                    <img src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/the-matrix-resurrections-et00133391-24-11-2021-12-59-51.jpg">
+                    </img>
+                </div>
+                <div>
+                    <h1>The Matrix Resurrections</h1>
+                    
+                </div>
+        </div>    
         </>
     );
 };
